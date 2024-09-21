6a8b39f647074918d9fcdd5e2768814cbee5947b
suvamlenka <151668438+suvamlenka@users.noreply.github.com>
2024-09-21 19:40:50 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -34,13 +34,13 @@ const Footer = () => {
           </h2>
           <ul className="text-gray-600">
             <li className="mb-1">
-              <Link to="/about">+91 99879 90097</Link>
+              <Link to="/about">+91 7325899029</Link>
             </li>
             <li className="mb-1">
               <Link to="/faq">+91 97649 35361</Link>
             </li>
             <li className="mb-1">
-              <Link to="/faq">findmynotes2022@gmail.com</Link>
+              <Link to="/faq">findmynotes2024@gmail.com</Link>
             </li>
           </ul>
         </div>
